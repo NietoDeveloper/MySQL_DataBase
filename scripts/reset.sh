679fac721e28ee96c4999184eac80ccfa8731710
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-08-03 23:38:37 -0500

    Update reset.sh

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -6,8 +6,3 @@ set -euo pipefail
 : "${MYSQL_HOST:?Set MYSQL_HOST}"
 : "${MYSQL_PORT:?Set MYSQL_PORT}"
 : "${MYSQL_USER:?Set MYSQL_USER (bootstrap/admin account)}"
-: "${MYSQL_PASSWORD:?Set MYSQL_PASSWORD}"
-
-
-read -p "This will DROP all data in '$MYSQL_DATABASE'. Type 'yes' to continue: " confirm
-if [ "$co
