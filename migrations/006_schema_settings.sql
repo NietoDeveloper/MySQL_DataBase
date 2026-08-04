707ad7e69ee474a6e4c99410c2a7d68dd6576420
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-08-03 23:41:17 -0500

    Update 006_schema_settings.sql

--- migrations/006_schema_settings.sql
+++ migrations/006_schema_settings.sql
@@ -8,8 +8,3 @@ CREATE TABLE IF NOT EXISTS app_settings (
     value       JSON NOT NULL,
     updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
                     ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS user_settings (
-
-    `key`       VARCHAR(128) NOT NULL,
