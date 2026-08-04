18ab5a19247a316713648c98d397a4d7f77ad693
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-08-03 23:41:23 -0500

    Update 006_schema_settings.sql

--- migrations/006_schema_settings.sql
+++ migrations/006_schema_settings.sql
@@ -5,6 +5,3 @@
 
 CREATE TABLE IF NOT EXISTS app_settings (
     `key`       VARCHAR(128) NOT NULL PRIMARY KEY,
-    value       JSON NOT NULL,
-    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-                    ON UPDATE CURRENT_TIMESTAMP
