add62113c2917ff8a8a88c46bba7e0a27cf2aef8
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-08-04 23:12:33 -0500

    Update 001_schema_core.sql

--- migrations/001_schema_core.sql
+++ migrations/001_schema_core.sql
@@ -1,6 +1,4 @@
 -- =====================================================================
--- 001_schema_core.sql
-
 CREATE TABLE IF NOT EXISTS users (
     id                    CHAR(36) NOT NULL DEFAULT (UUID()) PRIMARY KEY,
     email                 VARCHAR(255) NOT NULL,
