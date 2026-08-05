7bdd8e80b69d8024fc93fbce93b5df85c6465989
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-08-04 23:19:25 -0500

    Update 001_schema_core.sql

--- migrations/001_schema_core.sql
+++ migrations/001_schema_core.sql
@@ -11,6 +11,4 @@ CREATE TABLE IF NOT EXISTS users (
  -- do not rely on this instead of application-level validation).
     CONSTRAINT chk_users_email_format
     FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
- TABLE IF NOT EXISTS user_roles (
-    user_id CHAR(36) NOT NULL,
-    role_id SMALLINT UNSIGNED NOT N
+ role_id SMALLINT UNSIGNED
